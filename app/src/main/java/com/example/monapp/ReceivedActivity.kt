3e5efbe1a8fc6cc9b7127da0249753a21950aa9f
Hugo <hugobeno22@gmail.com>
2026-02-17 08:57:45 +0100

    Received Screen

--- app/src/main/java/com/example/monapp/ReceivedActivity.kt
+++ app/src/main/java/com/example/monapp/ReceivedActivity.kt
@@ -1,4 +1,4 @@
-package com.example.monapp
+package com.neac.basesandroid
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,8 +9,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,7 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.monapp.ui.theme.MonAppTheme
-import com.example.monapp.ui.theme.PurpleGrey40
+
 
 class ReceivedActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,34 +36,62 @@ class ReceivedActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MonAppTheme {
-                val countReceived = intent
-                    .getIntExtra("count", 0)
+                val countReceived = intent.getIntExtra("count", 0)
                 ReceivedScreen(countReceived)
-
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ReceivedScreen(countReceived : Int) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(
-            space = 30.dp,
-            alignment = Alignment.CenterVertically
-        ),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = PurpleGrey40)
-    ) {
+fun ReceivedScreen(countReceived : Int){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Received Screen",
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight
+                            .ExtraBold)
+                },
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = null,
+                        modifier = Modifier
+                    )
+                },
+                actions = {
+                    IconButton(
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Notifications,
+                            contentDescription = null,
+                            modifier = Modifier
+                        )
+                    }
+                    IconButton(
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Notifications,
+                            contentDescription = null,
+                            modifier = Modifier
+                        )
+                    }
+                }
+            )
+        }
+    ) { }
+}
 
-        Text(
-            text = "$countReceived",
-            fontSize = 45.sp,
-            fontWeight = FontWeight.ExtraBold,
-            color = Color.White
-        )
-
+@Preview
+@Composable
+private fun ReceivedScreenPreview() {
+    MonAppTheme {
+        ReceivedScreen(countReceived = 10)
     }
 }
